cc9e033e88d2e21d47378fd329e4c9412803eead
melody <877015767@qq.com>
2020-01-04 17:25:43 +0800

    project build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/weinihaigou/' : '/',
+  outputDir: 'dist',
+  productionSourceMap: false,
+  assetsDir: 'static',
+  filenameHashing: true,
+  pages: {
+    index: {
+      // page 的入口
+      entry: "src/main.js",
+      // 模板来源
+      template: "public/index.html", // 这里用来区分加载那个 html
+      // 在 dist/index.html 的输出
+      filename: "index.html",
+      // 在这个页面中包含的块，默认情况下会包含
+      // 提取出来的通用 chunk 和 vendor chunk。
+      chunks: ["chunk-vendors", "chunk-common", "index"]
+    }
+  },
   devServer: {
     port: 8080,
     disableHostCheck: true,
