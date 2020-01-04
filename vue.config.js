f9f43a506dc01090059f85516301372f8baeb51b
melody <877015767@qq.com>
2020-01-04 17:35:14 +0800

    project build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/weinihaigou/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   outputDir: 'dist',
   productionSourceMap: false,
   assetsDir: 'static',
