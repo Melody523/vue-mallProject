f66a47db4851efdfa1e9986fce1ae9b5bceb112e
melody <877015767@qq.com>
2020-01-04 20:56:55 +0800

    project build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-mallProject' : '/',
   outputDir: 'dist',
   productionSourceMap: true,
   assetsDir: 'static',
